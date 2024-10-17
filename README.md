76d882d3c55723dcc885ee9a646474a29afced5b
Daniel Boachie <dboachie222@gmail.com>
2024-10-16 20:01:31 -0400

    React Router

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# React-Router
+Deployed site link
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
